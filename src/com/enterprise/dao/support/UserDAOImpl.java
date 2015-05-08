2d68cea0fa58db5d119ce2dfae860be8a305c382
jelb123 <jelboustani95@gmail.com>
2015-05-08 22:41:27 +1000

    Fixed up more of Geoffs fucked upery

--- src/com/enterprise/dao/support/UserDAOImpl.java
+++ src/com/enterprise/dao/support/UserDAOImpl.java
@@ -8,70 +8,58 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.enterprise.beans.AddressBean;
-import com.enterprise.beans.ItemBean;
-import com.enterprise.beans.PriceBean;
 import com.enterprise.beans.UserBean;
 import com.enterprise.common.DBConnectionFactory;
 import com.enterprise.common.ServiceLocatorException;
 import com.enterprise.dao.DataAccessException;
-import com.enterprise.dao.ItemDAO;
+import com.enterprise.dao.UserDAO;
 
-public class ItemDAOImpl implements ItemDAO {
+public class UserDAOImpl implements UserDAO{
 	
-	/*
-	 * Initialisation of bullshit variables that are needed for connected to the Database
-	 */
 	private DBConnectionFactory services;
-	public ItemDAOImpl() {
+	
+	public UserDAOImpl() {
 		try {
 			services = new DBConnectionFactory();
 		} catch (ServiceLocatorException e) {
 			e.printStackTrace();
 		}
 	}
-	public ItemDAOImpl(DBConnectionFactory services) {
+	
+	public UserDAOImpl(DBConnectionFactory services) {
 		this.services = services;
 	}
-	
-	
-	
-	/*
-	 * This prepares the SQL statements that will read user input and convert it into SQL insert statements
-	 * into the AUCTION_ITEM table
-	 */
+
 	@Override
-	public void insert(ItemBean itemBean) throws DataAccessException {
+	public void insert(UserBean userBean) throws DataAccessException {
 		try {
 			Connection con = services.createConnection();
 			PreparedStatement ps = con.prepareStatement(
-				"insert into TBL_ITEMS (item_id, ownerID, title, category, " +
-				"picture, description, address, reservePrice, startPrice, " + 
-				"bidIncrements, endTime, highestBid, highest_bid_user_ID)" +
-				" values (?,?,?,?,?,?,?,?,?,?,?,?,?)");
+				"insert into TBL_USERS (username, password, email, nickname, " +
+				"firstname, lastname, date_of_birth, street_address, city, " + 
+				"state, country, postcode, credit_card_number, account_state, admin)" +
+				" values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
 			
-			/*
-			 * read values in from input form
-			 */
-			//ps.setInt(11, itemBean.getId()); the item_id is automatically generated
+			ps.setString(1, userBean.getUsername());
+			ps.setString(2, userBean.getPassword());
+			ps.setString(3, userBean.getEmail());
+			ps.setString(4, userBean.getNickname());
+			ps.setString(5, userBean.getFirstName());
+			ps.setString(6, userBean.getLastName());
+			ps.setString(7, userBean.getDateOfBirth());
+			ps.setString(8, userBean.getAddress().getStreetAddress());
+			ps.setString(9, userBean.getAddress().getCity());
+			ps.setString(10, userBean.getAddress().getState());
+			ps.setString(11, userBean.getAddress().getCountry());
+			ps.setInt(12, userBean.getAddress().getPostCode());
+			ps.setInt(13, userBean.getCreditCardNumber());
+			ps.setInt(14, userBean.getAccountState());
+			ps.setBoolean(15, userBean.isAdmin());
 			
-			ps.setInt(1, itemBean.getItemId());
-			ps.setInt(2, itemBean.getOwnerId());
-			ps.setString(3, itemBean.getTitle());
-			ps.setString(4, itemBean.getCategory());
-			ps.setString(5, itemBean.getPicture());
-			ps.setString(6, itemBean.getDescription());
-			ps.setString(7, itemBean.getAddress().getStreetAddress());
-			ps.setFloat(8, itemBean.getReservePrice().getPrice());
-			ps.setFloat(9, itemBean.getStartPrice().getPrice());
-			ps.setFloat(10, itemBean.getBidIncrements());
-			ps.setString(11, itemBean.getEndTime());
-			ps.setFloat(12, itemBean.getHighestBid());
-			ps.setInt(13, itemBean.getHighestBidUserId());
-	
 			
 			int rows = ps.executeUpdate();
 			if (rows < 1) 
-				throw new DataAccessException("TtemBean: " + itemBean + " not inserted");
+				throw new DataAccessException("UserBean: " + userBean + " not inserted");
 		} catch (ServiceLocatorException e) {
 			e.printStackTrace();
 			throw new DataAccessException("UserBean: " + userBean + " not inserted");
@@ -79,23 +67,21 @@ public class ItemDAOImpl implements ItemDAO {
 			e.printStackTrace();
 			throw new DataAccessException("UserBean: " + userBean + " not inserted");
 		}
+		
 	}
 
-	/*
-	 * delete item that is pointed to by the item_id that was passed in
-	 * 
-	 */
+	@Override
 	public void delete(int id) throws DataAccessException {
 		Connection con = null;
 		PreparedStatement ps = null;
 		try {
 			con = services.createConnection();
 			ps = con.prepareStatement(
-				"delete from TBL_ITEMS where item_id=?");
+				"delete from TBL_USERS where id=?");
 			ps.setInt(1, id);
 			int rows = ps.executeUpdate();
 			if (rows < 1) 
-				throw new DataAccessException("ItemBean: " + id + " not deleted");
+				throw new DataAccessException("UserBean: " + id + " not deleted");
 		} catch (ServiceLocatorException e) {
 			e.printStackTrace();
 		} catch (SQLException e) {
@@ -105,25 +91,52 @@ public class ItemDAOImpl implements ItemDAO {
 			close(con);
 		}
 	}
+
+	@Override
+	public UserBean findByLoginDetails(String username, String password) throws DataAccessException {
+		Connection con = null;
+		try {
+			con = services.createConnection();
+			PreparedStatement stmt = con.prepareStatement("select * from TBL_USERS where username=? and password=?");
+			stmt.setString(1, username);
+			stmt.setString(2, password);
+			ResultSet rs = stmt.executeQuery();
+			if (rs.next()) {
+				UserBean user = createUserBean(rs);
+				stmt.close(); 
+				rs.close();
+				return user;
+			}
+		} catch (ServiceLocatorException e) {
+			throw new DataAccessException("Unable to retrieve connection; " + e.getMessage(), e);
+		} catch (SQLException e) {
+			throw new DataAccessException("Unable to execute query; " + e.getMessage(), e);
+		} finally {
+			if (con != null) {
+				try {
+					con.close();
+				} catch (SQLException e1) {
+					e1.printStackTrace();
+				}
+			}
+		}
+		return null;
+	}
 	
-	
-	/*
-	 * output the whole list of items in the database
-	 */
-	public List<ItemBean> showAllItems() throws DataAccessException {
-		List<ItemBean> list = new ArrayList<ItemBean>();
+	public List<UserBean> findAllUsers() throws DataAccessException {
+		List<UserBean> list = new ArrayList<UserBean>();
 		Connection con = null;
 		PreparedStatement ps = null;
 		ResultSet rs = null;
 		try {
 			con = services.createConnection();
 			ps = con.prepareStatement(
-					"select * from TBL_ITEMS");
+					"select * from TBL_USERS");
 			rs = ps.executeQuery();
 			while (rs.next())
-				list.add(createItemBean(rs));
+				list.add(createUserBean(rs));
 		} catch (SQLException e) {
-			throw new DataAccessException("Unable to find all items", e);
+			throw new DataAccessException("Unable to find all users", e);
 		} catch (ServiceLocatorException e) {
 			throw new DataAccessException("Unable to locate connection", e);
 		} finally {
@@ -134,156 +147,58 @@ public class ItemDAOImpl implements ItemDAO {
 		return list;
 	}
 	
-	
-	/*
-	 * Finds and returns the itemBean that contains "string/catagory/desc"
-	 */
-	public List<ItemBean> findItemByString(String searchString){
-		/*
-		int i, found;
-		List<ItemBean> list = new ArrayList<ItemBean>();
-		ItemBean item = new ItemBean();
-		
-		//for now just get a list of all the items and iterate through it
-		list = showAllItems();												
-		for (i = 0, found = 0; i < list.size(); i++){
-			item = list.get(i);
-			
-			//check all substrings
-			if(item.getTitle().contains(searchString) ){
-				found = 1;
-			}
-			if(item.getCategory().contains(searchString)){
-				found = 1;
-			}
-			if(item.getDescription().contains(searchString)){
-				found = 1;
-			}
-			if(found == 0){
-				//remove the item from the list if it wasn't found
-				list.remove(i);
-				i--;
-			}
-		}
-		*/
-		
-		List<ItemBean> list = new ArrayList<ItemBean>();
+	public UserBean findUserByID(int id) throws DataAccessException {
+		UserBean user = null;
 		Connection con = null;
 		PreparedStatement ps = null;
 		ResultSet rs = null;
 		try {
 			con = services.createConnection();
-			
-			//CHECK IF CORRECT %?% might throw errors hard here!!
-			ps = con.prepareStatement(
-					"select * from TLB_ITEMS where title like %?% or description like %?% or category like %?%");
-			ps.setString(1, searchString);
-			ps.setString(2, searchString);
-			ps.setString(3, searchString);
-			
-			rs = ps.executeQuery();
-			while (rs.next())
-				list.add(createItemBean(rs));
-		} catch (SQLException e) {
-			throw new DataAccessException("Unable to find all items", e);
-		} catch (ServiceLocatorException e) {
-			throw new DataAccessException("Unable to locate connection", e);
-		} finally {
-			close(rs);
-			close(ps);
-			close(con);
-		}
-		
-		return list;
-	}
-	
-	
-	
-	public ItemBean getItemById(int id){
-		ItemBean item = new ItemBean;
-		item = null;
-		Connection con = null;
-		PreparedStatement ps = null;
-		ResultSet rs = null;
-		try {
-			con = services.createConnection();
-			ps = con.prepareStatement(
-					"select * from TLB_ITEMS where item_id = ?");
+			ps = con.prepareStatement("select * from TBL_USERS where id = ?");
 			ps.setInt(1, id);
 			rs = ps.executeQuery();
-	
-		} catch (SQLException e) {
-			throw new DataAccessException("Unable to find item pointed by id", e);
+			if (!rs.next())
+				return null;
+			user = createUserBean(rs);
 		} catch (ServiceLocatorException e) {
-			throw new DataAccessException("Unable to locate connection", e);
+			throw new DataAccessException("Unable to create user from database; " + e.getMessage(), e);
+		} catch (SQLException e) {
+			throw new DataAccessException("SQLException while creating contact from database;" + e.getMessage(), e);
 		} finally {
 			close(rs);
 			close(ps);
 			close(con);
 		}
-		return item;
+		return user;
 	}
 	
-	
-	/*
-	 * updating to the new highest bidder value
-	 */
-	public void updateBid(int item_id, float bid_value, int bidder_id){
-		Connection con = null;
-		PreparedStatement ps = null;
-		ResultSet rs = null;
-		try {
-			con = services.createConnection();
-			ps = con.prepareStatement(
-					"update TLB_ITEMS "
-					+ "set  "where item_id = ?");
-			ps.setInt(1, id);
-			rs = ps.executeQuery();
-	
-		} catch (SQLException e) {
-			throw new DataAccessException("Unable to find item pointed by id", e);
-		} catch (ServiceLocatorException e) {
-			throw new DataAccessException("Unable to locate connection", e);
-		} finally {
-			close(rs);
-			close(ps);
-			close(con);
-		}
-		
-	}
-	
-	
-	
-	private ItemBean createItemBean(ResultSet rs) throws SQLException {
-		ItemBean item = new ItemBean();
+	private UserBean createUserBean(ResultSet rs) throws SQLException {
+		UserBean user = new UserBean();
 		AddressBean address = new AddressBean();
-		PriceBean price = new PriceBean();
 		
-		//Grab address of item and package it up
-		address.setStreetAddress(rs.getString("address"));
-		item.setAddress(address);
+		//Grab address of user
+		address.setStreetAddress(rs.getString("street_address"));
+		address.setCity(rs.getString("city"));
+		address.setState(rs.getString("state"));
+		address.setCountry(rs.getString("country"));
+		address.setPostCode(rs.getInt("postcode"));
 		
-		//Grab and set item details
-		item.setItemId(rs.getInt("item_id"));
-		item.setOwnerId(rs.getInt("ownerID"));
-		item.setTitle(rs.getString("title"));
-		item.setCategory(rs.getString("category"));
-		item.setPicture(rs.getString("picture"));
-		item.setDescription(rs.getString("description"));
+		//Grab and set user details
+		user.setId(rs.getInt("id"));
+		user.setUsername(rs.getString("username"));
+		user.setPassword(rs.getString("password"));
+		user.setEmail(rs.getString("email"));
+		user.setNickname(rs.getString("nickname"));
+		user.setFirstName(rs.getString("firstname"));
+		user.setLastName(rs.getString("lastname"));
+		user.setDateOfBirth(rs.getString("date_of_birth"));
+		user.setAddress(address);
+		user.setCreditCardNumber(rs.getInt("credit_card_number"));
+		user.setAdmin(rs.getBoolean("admin"));
+		user.setAccountState(rs.getInt("account_state"));
 		
-		//Grab price of item and package to Bean
-		price.setPrice(rs.getFloat("reservePrice"));
-		price.setPrice(rs.getFloat("startPrice"));
-		item.setReservePrice(price);
-		
-		item.setBidIncrements(rs.getFloat("bidIncrements"));
-		item.setEndTime(rs.getString("endTime"));
-		item.setHighestBid(rs.getFloat("highestBid"));
-		item.setHighestBidUserId(rs.getInt("highest_bid_user_ID"));
-		
-		return item;
+		return user;
 	}
-	
 	
 	private void close (Connection con) {
 		try {
@@ -293,7 +208,6 @@ public class ItemDAOImpl implements ItemDAO {
 			e.printStackTrace();
 		}
 	}
-	
 	private void close(ResultSet rs) {
 		try {
 			if (rs != null)
@@ -311,5 +225,5 @@ public class ItemDAOImpl implements ItemDAO {
 			e.printStackTrace();
 		}
 	}
-	
+
 }
