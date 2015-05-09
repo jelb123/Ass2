6c2e846c5b72be1992e1c0339ec4de945cf5fc6a
gcho300 <gcho300@cse.unsw.EDU.AU>
2015-05-09 12:43:27 +1000

    updated the interface for ItemDAO

--- src/com/enterprise/dao/ItemDAO.java
+++ src/com/enterprise/dao/ItemDAO.java
@@ -1,5 +1,7 @@
 package com.enterprise.dao;
 
+import java.util.List;
+
 import com.enterprise.beans.ItemBean;
 
 
@@ -23,4 +25,41 @@ public interface ItemDAO {
 	 */
 	void delete(int id) throws DataAccessException;
 	
+	/**
+	 * Returns a list of all items in the database
+	 * @return a list<ItemBean>
+	 * @throws DataAccessException
+	 */
+	public List<ItemBean> showAllItems() throws DataAccessException;
+	
+	/**
+	 * This is used when "search" button is activated, the input string is searched in the database
+	 * if it is contained in "title" "category" "description"
+	 * @param searchString
+	 * @return a list<ItemBean> that contains "searchString" input
+	 */
+	public List<ItemBean> findItemByString(String searchString);
+	
+	/**
+	 * Simply retrieves the itemBean that is equal to the id passed in (item_id)
+	 * @param id refers to the item_id
+	 * @return itemBean that contains this item_id
+	 */
+	public ItemBean getItemById(int id);
+	
+	/**
+	 * Updates the database item if and only if the NEW bid_value > then the CURRENT big_value in the database  
+	 * @param item_id
+	 * @param bid_value		new highest bid value
+	 * @param bidder_id 	the user_id of the new highest bidder
+	 * @return if we update the value (ie. bid_value > current db_bid_value), ret = 1 else ret = 0
+	 */
+	public int updateBid(int item_id, float bid_value, int bidder_id);
+	
+	/**
+	 * To be written and determined how to be handled
+	 * @param item_id
+	 * @param upTime
+	 */
+	public void haltAuction(int item_id, int upTime);
 }
